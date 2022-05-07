e28175dcedc2c024462940dd7e4a1fb350daa2f8
Lukas <Lukas.Erekson@gmail.com>
2022-05-07 10:56:40 -0600

    Create path for initialized server object

--- lib/MCServer.rb
+++ lib/MCServer.rb
@@ -3,6 +3,15 @@
 require_relative "MCServer/version"
 
 module MCServer
-  class Error < StandardError; end
-  # Your code goes here...
+  # class Error < StandardError; end
+  # # Your code goes here...
+
+  class MCServer
+    attr_reader :pid, :path
+
+    def initialize(path = "~/Minecraft_Server")
+      @path = path
+      @pid = nil
+    end
+  end
 end
