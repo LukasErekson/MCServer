b4fdb462cdab3ccd98a2edba8b469c92e2d3d15e
Lukas <Lukas.Erekson@gmail.com>
2022-05-07 20:58:28 -0600

    Begin skeleton of #open and #close
    - Implement ServerAlreadyRunningError
    - Fork a new process in #start and kill process in #close

--- lib/MCServer.rb
+++ lib/MCServer.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require 'open3'
+
 require_relative "MCServer/version"
 require_relative "MCServer/server_not_started_error"
+require_relative "MCServer/server_already_running_error"
 
 module MCServer
   class MCServer
@@ -14,11 +17,49 @@ module MCServer
     end
 
     def active?
-      @active
+      return @active = false if @pid.nil?
+
+      @active = !!`ps -p #{@pid}`["\n"]
+    end
+
+    def start
+      raise ServerAlreadyRunningError "A server instance is already running" if active?
+      @read_io, @write_io = IO.pipe
+
+      @pid = fork do
+        STDIN.reopen(@read_io)
+        STDOUT.reopen(@write_io)
+
+        puts "Start server"
+      end
+
+      Process.detach(@pid)
+
+
+      unless active?
+        @pid = nil
+        close
+      end
+
+      true
+    end
+
+    def close
+      raise ServerNotStartedError "Cannot close the server; the server is not started" unless active?
+
+      `kill #{@pid.to_i}` unless @pid.nil?
+      @read_io.close
+      @write_io.close
+
+      @pid = nil
+
+      true
     end
 
     def command(_command_str)
       raise ServerNotStartedError "Cannot send command; the server is not started" unless active?
     end
   end
+
+  
 end
---
+++ lib/MCServer/server_already_running_error.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+class ServerAlreadyRunningError < StandardError; end
