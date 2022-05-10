a10463807b3b2537544dd0bda8fb78f09039d7a0
Lukas <Lukas.Erekson@gmail.com>
2022-05-09 21:26:02 -0600

    Refactor #start and #close to safely exit
    - Send "/stop" to the server instead of killing the process,
      ensuring that the world saves instead of just stopping it
    - Split out the starting command to its own method

--- lib/mc_server/minecraft_server.rb
+++ lib/mc_server/minecraft_server.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Minecraft_Server
-  attr_reader :pid, :path, :active
+  attr_reader :pid, :path, :active, :read_io, :write_io
 
   def initialize(path = "#{Dir.home}/Minecraft_Server")
     @path = path
@@ -20,14 +20,7 @@ class Minecraft_Server
       raise ServerAlreadyRunningError "A server instance is already running. To start antoher server, please create a new object instance"
     end
 
-    @read_io, @write_io = IO.pipe
-
-    @pid = fork do
-      $stdin.reopen(@read_io)
-      $stdout.reopen(@write_io)
-
-      puts "Start server"
-    end
+    @pid = start_command
 
     Process.detach(@pid)
 
@@ -42,7 +35,7 @@ class Minecraft_Server
   def close
     raise ServerNotStartedError "Cannot close the server; the server is not started" unless active?
 
-    `kill #{@pid.to_i}` unless @pid.nil?
+    @write_io.puts "/stop"
     @read_io.close
     @write_io.close
 
@@ -53,5 +46,20 @@ class Minecraft_Server
 
   def command(_command_str)
     raise ServerNotStartedError "Cannot send command; the server is not started" unless active?
+  end
+
+  protected
+
+  def start_command
+    @read_io, @write_io = IO.pipe
+
+    Dir.chdir(path)
+
+    fork do
+      $stdin.reopen(@read_io)
+      $stdout.reopen(@write_io)
+
+      `java -jar #{path}/server.jar --nogui`
+    end
   end
 end
