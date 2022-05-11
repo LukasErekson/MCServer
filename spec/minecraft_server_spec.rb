299bb283e9f779d0c8b81752c471b83a35c2e2a6
Lukas <Lukas.Erekson@gmail.com>
2022-05-10 20:50:51 -0600

    Stop tests from running server instances
    - Use stubs to prevent Rspec from opening serval Minecraft
      server instances each run; now it will only create 1 to test
      #command

--- spec/minecraft_server_spec.rb
+++ spec/minecraft_server_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Minecraft_Server do
     end
 
     it "has a default path of ~/Minecraft_Server" do
-      expect(server.path).to match /\/home\/.*\/Minecraft_Server/
+      expect(server.path).to match(%r{/home/.*/Minecraft_Server})
     end
 
     it "defaults active to false" do
@@ -19,10 +19,51 @@ RSpec.describe Minecraft_Server do
     end
   end
 
-  describe "#start" do
-    context "when the server has not been started" do
+  describe "#command" do
+    context "when given an inactive server" do
+      let(:server) { Minecraft_Server.new }
+
+      it "raises a ServerNotStartedError" do
+        expect { server.command "/list" }.to raise_error(proc { ServerNotStartedError.new })
+      end
+    end
+
+    context "when given an active server" do
       before(:all) do
         @server = Minecraft_Server.new
+        @server.start
+      end
+
+      it "the server receives the command" do
+        sleep(1) until @server.log.readline.include? "Done"
+
+        @server.command "/list"
+        @server.log.rewind
+
+        reg_ex = /.*There are 0 of a max of \d+ players online: \n/
+        expect(@server.log.readlines.any? { |line| line =~ reg_ex }).to be true
+      end
+
+      it "returns true for a valid sent command" do
+        expect(@server.command("/list")).to be true
+      end
+
+      it "returns false for an invalid command" do
+        expect(@server.command("give me diamonds")).to be false
+      end
+
+      after(:all) do
+        @server.stop
+      end
+    end
+  end
+
+  describe "#start" do
+    context "when the server has not been started" do
+      before(:each) do
+        @server = Minecraft_Server.new
+        allow_any_instance_of(Minecraft_Server).to receive(:start_command).and_return(2)
+        @server.instance_eval("@read_io, @write_io = IO.pipe", __FILE__, __LINE__)
         @success = @server.start
       end
 
@@ -40,23 +81,26 @@ RSpec.describe Minecraft_Server do
         expect(@server.active?).to be true
       end
 
-      after(:all) do
+      after(:each) do
         @server.stop
       end
     end
 
     context "when a sever is already started" do
-      before(:all) do
+      before(:each) do
         @server = Minecraft_Server.new
-        @server.start
+        allow_any_instance_of(Minecraft_Server).to receive(:start_command).and_return(2)
+        @server.instance_eval("@read_io, @write_io = IO.pipe", __FILE__, __LINE__)
+        allow(@server).to receive(:active?).and_return true
       end
 
       it "raises a ServerAlreadyRunning error" do
         expect { @server.start }.to raise_error(proc { ServerAlreadyRunningError.new })
       end
 
-      after(:all) do
-        @server.stop
+      after(:each) do
+        @server.instance_eval("@read_io.close", __FILE__, __LINE__)
+        @server.instance_eval("@write_io.close", __FILE__, __LINE__)
       end
     end
   end
@@ -65,6 +109,8 @@ RSpec.describe Minecraft_Server do
     context "when the server is running" do
       before(:each) do
         @server = Minecraft_Server.new
+        allow_any_instance_of(Minecraft_Server).to receive(:start_command).and_return(2)
+        @server.instance_eval("@read_io, @write_io = IO.pipe", __FILE__, __LINE__)
         @server.start
       end
 
@@ -90,16 +136,6 @@ RSpec.describe Minecraft_Server do
 
       it "raises a ServerNotStartedError" do
         expect { server.stop }.to raise_error(proc { ServerNotStartedError.new })
-      end
-    end
-  end
-
-  describe "#command" do
-    context "when given an inactive server" do
-      let(:server) { Minecraft_Server.new }
-
-      it "raises a ServerNotStartedError" do
-        expect { server.command "/list" }.to raise_error(proc { ServerNotStartedError.new })
       end
     end
   end
