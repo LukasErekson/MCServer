5075269665a100f317ca591425f40ad9a5124aeb
Lukas <Lukas.Erekson@gmail.com>
2022-05-16 21:33:36 -0600

    Allow #command to receive "/stop" and call  #stop

--- lib/mc_server/minecraft_server.rb
+++ lib/mc_server/minecraft_server.rb
@@ -52,6 +52,9 @@ class Minecraft_Server
     raise ServerNotStartedError "Cannot send command; the server is not started" unless active?
 
     root_command = command_str.split[0]
+
+    return stop if root_command == "/stop"
+
     return false unless MC_Server::VALID_COMMANDS.include? root_command
 
     @write_io.puts command_str
